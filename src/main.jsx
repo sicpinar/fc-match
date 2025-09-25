11035341066b6840972765a4b5bcd50b562cd66a
sicpinar <s.icpinar@googlemail.com>
2025-09-25 23:46:24 +0200

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+// Projekt A: React App Bootstrap
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
@@ -11,3 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>,
 )
+
+// EXT-HOOK: Service Worker Updates
+// EXT-HOOK: Error Boundary für Projekt D
